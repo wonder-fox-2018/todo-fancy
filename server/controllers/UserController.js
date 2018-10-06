41775784f5b0c1e950b1beda0d433356515b062a
esadeli <efrat.sadeli@gmail.com>
2018-10-06 09:21:04 +0700

    User Login Server OK

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -33,7 +33,7 @@ class UserController{
                         })
                     }else{
                         res.status(500).json({
-                            msg: 'ERROR: ',err
+                            msg: 'ERROR TOKEN: ',err
                         })
                     }
                 })
@@ -52,7 +52,48 @@ class UserController{
 
     // login user
     static loginUser(req,res){
-
+        // check email
+        if(isEmail(req.body.email)){
+            let hash = hashPassword(req.body.password)
+            User.findOne({
+                email: req.body.email,
+                password: hash
+            })
+            .then(user=>{
+                if(user){
+                    // get the token
+                    jwt.sign({
+                        userid: user._id,
+                        name: user.name,
+                        email: user.email
+                    },process.env.SECRETTOKEN, (err, token)=>{
+                        if(!err){
+                            res.status(200).json({
+                                msg: 'User Login Success',
+                                token: token
+                            })
+                        }else{
+                            res.status(500).json({
+                                msg: 'ERROR TOKEN: ',err
+                            })
+                        }
+                    })
+                }else if(user=== null){
+                    res.status(400).json({
+                        msg: 'ERROR Login: User not found'
+                    })
+                }
+            })
+            .catch(error =>{
+                res.status(500).json({
+                    msg: 'ERROR: ',error
+                })
+            })
+        }else{
+            res.status(500).json({
+                msg: 'ERROR: Please check your email'
+            })
+        }
     }
 
     // login with Google
