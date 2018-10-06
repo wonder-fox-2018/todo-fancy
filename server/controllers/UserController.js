ba753ce0315af2294b16aa98c6654883d6941a5e
esadeli <efrat.sadeli@gmail.com>
2018-10-06 20:27:23 +0700

    Login/Register via Google Server-client OK

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,8 +2,11 @@
 
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
+const axios = require('axios')
 const hashPassword = require('../helpers/hashPassword')
 const isEmail = require('../helpers/isEmail')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 class UserController{
 
@@ -98,7 +101,103 @@ class UserController{
 
     // login with Google
     static loginGoogle(req,res){
-
+        return new Promise((resolve,reject)=>{
+            client.verifyIdToken({
+              idToken: req.body.googletoken,
+              audience: process.env.GOOGLE_CLIENT_ID
+            },(err,result)=>{
+                if(!err){
+                    // console.log('RESULT--->', result)
+                    // const payload = result.getPayload()
+                    // const userid = payload['sub']
+                    resolve('anything') // replace with anything instead of userid
+                }else{
+                    reject(err)
+                }
+            })
+        })
+         .then(accessok =>{
+            // gain the data from Google
+            axios({
+               method: 'GET',
+               url: `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${req.body.googletoken}` 
+            })
+              .then(result=>{
+                // console.log('HASIL DATA--->', result.data)
+                // check the user first
+                User.findOne({
+                   email: result.data.email  
+                })
+                 .then(user=>{
+                    if(user){
+                        // get the jwt token
+                        jwt.sign({
+                            userid: user._id,
+                            name: user.name,
+                            email: user.email
+                        },process.env.SECRETTOKEN, (err,token)=>{
+                            if(!err){
+                              res.status(200).json({
+                                 msg: 'User Login via Google Success',
+                                 token: token
+                              })
+                            }else{
+                              res.status(500).json({
+                                  msg: 'ERROR TOKEN: ',err
+                              })
+                            }
+                        })
+                    }else if(user===null){
+                        //create new user
+                        let hash = hashPassword(process.env.DEFAULT_PASSWORD)
+                        User.create({
+                          name: result.data.name,
+                          email: result.data.email,
+                          password: hash,
+                          thirdpartylogin: 'YES'
+                        })
+                         .then(user=>{
+                            jwt.sign({
+                               userid: user._id,
+                               name: user.name,
+                               email: user.email 
+                            },process.env.SECRETTOKEN, (err,token)=>{
+                               if(!err){
+                                  res.status(200).json({
+                                     msg: 'User Registration via Google Success',
+                                     token: token
+                                  })
+                                }else{
+                                    res.status(500).json({
+                                        msg: 'ERROR TOKEN: ',err
+                                    })
+                                }
+                           })
+                         })
+                         .catch(error =>{
+                            res.status(500).json({
+                                msg: 'ERROR: ',error
+                            })         
+                         })
+                    }
+                 })
+                 .catch(error=>{
+                    res.status(500).json({
+                        msg: 'ERROR: ',error
+                    })     
+                 })
+              })
+              .catch(error=>{
+                res.status(500).json({
+                    msg: 'ERROR Get Data Google Login: ',error
+                })      
+              })
+         })
+         .catch(error =>{
+            res.status(500).json({
+                msg: 'ERROR Verify Google TOKEN Google Login: ',error
+            })
+         })
     }
 
     // get info of user by email
