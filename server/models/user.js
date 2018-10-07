f1fb54b4350b6bd80be22e6f2aae104fc5f89dcb
pputra <mynameisyoga@live.com>
2018-10-08 00:17:56 +0700

    add email validtor

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var isEmail = function(val) {
+    
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    return re.test(val);
+}
+
+
 const userSchema = new Schema({
     email: {
         type: String,
         required: [true, 'Email is required'],
-        unique: [true, 'An Email with that address has already been registered, please use another one']
+        unique: [true, 'An Email with that address has already been registered, please use another one'],
+        validate: isEmail
     },
     password: {
         type: String
