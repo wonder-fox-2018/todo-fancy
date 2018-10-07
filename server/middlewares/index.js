3760b73e936aa65adf96c02fb1bd197c719b4db5
pputra <mynameisyoga@live.com>
2018-10-07 23:40:54 +0700

    improve middleware security

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -11,22 +11,27 @@ module.exports = {
             
             req.decoded = decoded;
             
-            next();
+            User.findById(req.decoded.id).then((result) => {
+                next();
+            }).catch((err) => {
+                ServerResponse.error(res, 401, {message: 'user is not authorized'});
+            });
+            
+            
         } catch (error) {
             ServerResponse.error(res, 401, {message: 'Please login first'});
         }
     },
 
     hasAuthorization: (req, res, next) => {
-        console.log('masuk');
+        
         User.findById(req.decoded.id).populate('todo').exec().then((user) => {
-            ;
+            
             let hasAuthorization = false;
             
             user.todo.forEach(task => {
                 
                 if (task._id == req.body.id) {
-                    console.log('is authorized');
                     hasAuthorization = true;
                 }
             });
